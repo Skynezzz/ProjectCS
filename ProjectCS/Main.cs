0492c2c86b89c61a5717d683dcdbd58bccad5295
Samantha SCHNEIBEL <samantha.schneibel@hotmail.com>
2024-03-19 13:57:56 +0100

    pokemons

--- ProjectCS/Main.cs
+++ ProjectCS/Main.cs
@@ -1,15 +1,34 @@
 ﻿using Engine;
 using Engine.Entities;
 using Engine.Entities.Components;
+using Attacks;
 
 class Sakimon
 {
+    List<Attack> attackList;
+    Game game;
+    private void Init()
+    {
+        game = new Game();
+        InitAttacks();
+        Pokemons Pikachu = Pokemon();
+    }
+
+    private void InitAttacks()
+    {
+        attackList.add(new Attack("Vive Attaque", 0, 10, 20));
+        attackList.add(new Attack("Queue de Fer", 1, 15, 40));
+    }
+
+
     static void Main(string[] args)
     {
-        Game game = new Game();
-
+        Init();
         game.Run();
         Entity entity = new Entity();
         Drawable component = new Drawable();
+
     }
+
+
 }
