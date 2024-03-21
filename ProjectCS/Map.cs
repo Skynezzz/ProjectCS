34369e2ddfe0352e8c76469f8634b758c6dcd75c
Samantha SCHNEIBEL <samantha.schneibel@hotmail.com>
2024-03-21 19:41:20 +0100

    map

--- ProjectCS/Map.cs
+++ ProjectCS/Map.cs
@@ -1,53 +1,62 @@
-using System;
+ï»¿using System;
+using System.Numerics;
 using Engine.Entities;
+using Engine.Entities.Components;
+using Engine.Utils;
 
 namespace Sakimon.Entities.Map
 {
     class Map : Entity
     {
-        public void DrawMap()
+
+        public void ReadMap()
         {
-            // Définir les caractères pour les différents éléments de la carte
-            char playerChar = '@';
-            char wallChar = ' ';
-            char groundChar = ' ';
 
-            // Définir les couleurs pour les différents éléments de la carte
-            ConsoleColor playerColor = ConsoleColor.Red;
-            ConsoleColor wallColor = ConsoleColor.Gray;
-            ConsoleColor groundColor = ConsoleColor.Green;
+            string maptxt = Utils.GetTextFromFile("Map.txt");
+            string[] map = maptxt.Split('\n');
 
-            // Définir la taille de la carte
-            int mapWidth = Console.LargestWindowWidth;
-            int mapHeight = Console.LargestWindowHeight;
-
-            // Dessiner la carte
-            for (int y = 0; y < mapHeight; y++)
+            for (int y = 0; y < map.Length; y++)
             {
-                for (int x = 0; x < mapWidth; x++)
+                for (int x = 0; x < map[y].Length; x++)
                 {
-                    // Vérifier les coordonnées pour dessiner le joueur, les murs et le sol
-                    if (x == 1 && y == 1)
+                    switch (map[y][x])
                     {
-                        Console.ForegroundColor = playerColor;
-                        Console.Write(playerChar);
-                    }
-                    else if (x == 0 || x == mapWidth - 1 || y == 0 || y == mapHeight - 1)
-                    {
-                        Console.BackgroundColor = wallColor;
-                        Console.Write(wallChar);
-                    }
-                    else
-                    {
-                        Console.BackgroundColor = groundColor;
-                        Console.Write(groundChar);
+                        case 'T':
+                            Tree tree = new Tree(x, y);
+                            break;
+                        case 'H':
+                            House house = new House(x, y);
+                            break;
                     }
                 }
-                // Passer à la ligne suivante pour dessiner la prochaine rangée de la carte
-                Console.WriteLine();
             }
+        }
 
-            Console.ResetColor(); // Réinitialiser la couleur de la console
+    }
+
+    class MapEntity : Entity
+    {
+        public MapEntity(int x, int y)
+        {
+
+        }
+    }
+
+    class Tree : MapEntity
+    {
+        public Tree(int x, int y) : base(x, y)
+        {
+            AddComponent(new Drawable("ÆMM3\nÆYY3\n || "));
+        }
+    }
+
+    class House : MapEntity
+    {
+        public House(int x, int y) : base(x, y)
+        {
+            string housetxt = Utils.GetTextFromFile("House.txt");
+            string[] house = housetxt.Split('\n');
+            AddComponent(new Drawable(""));
         }
 
     }
