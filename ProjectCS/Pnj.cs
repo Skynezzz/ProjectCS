b10b30a306d9bcf90c9f7852c698aa4ede1a16fc
Skynezzz <117281599+Skynezzz@users.noreply.github.com>
2024-03-28 15:03:33 +0100

    pnj

---
+++ ProjectCS/Pnj.cs
@@ -0,0 +1,33 @@
+﻿using Engine;
+using Engine.Entities;
+using Engine.Entities.Components;
+using Engine.Utils;
+
+namespace Sakimon.Entities
+{
+
+    class PnjEntity : Entity
+    {
+        public PnjEntity(int x, int y)
+        {
+            AddComponent(new Position(x, y));
+            AddComponent(new Drawable("Assets/Pnj.txt", GetComponent<Position>()));
+            AddComponent(new Collider(0, 0, 3, 3));
+            Dialogue dialogue = new Dialogue(text, x, y);
+        }
+    }
+
+    class Dialogue : Entity
+    {
+        public Dialogue(string text, int x, int y)
+        {
+            AddComponent(new Position(x, y));
+            AddComponent(new Drawable("Assets/DialogueBox.txt", GetComponent<Position>()));
+            Drawable drawable = new Drawable();
+            drawable.SetShapeWithString(text);
+            AddComponent(drawable);
+        }
+
+    }
+
+}
